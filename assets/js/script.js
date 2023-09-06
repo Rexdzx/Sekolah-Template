24e0986bcb547c60ea5ddd7e560d1d536c557a68
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-09-06 16:57:57 +0700

    tambah halaman jurusan dan fix responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,30 @@
           aos_init();
       });
 
+      let navbarlinks = document.querySelectorAll('#navbar .scrollto');
+
+      function navbarlinksActive() {
+          navbarlinks.forEach(navbarlink => {
+
+              if (!navbarlink.hash) return;
+
+              let section = document.querySelector(navbarlink.hash);
+              if (!section) return;
+
+              let position = window.scrollY;
+              if (navbarlink.hash != '#header') position += 200;
+
+              if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                  navbarlink.classList.add('active');
+              } else {
+                  navbarlink.classList.remove('active');
+              }
+          })
+      }
+      window.addEventListener('load', navbarlinksActive);
+      document.addEventListener('scroll', navbarlinksActive);
+
+
       const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
       if (mobileNavToogle) {
           mobileNavToogle.addEventListener('click', function (event) {
